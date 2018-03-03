8130439cb8fc7939399c85d2eac166472ccdc0bb
DESKTOP-MI16EEP\Kristin AB <misskristin12@hotmail.com>
2018-03-03 16:33:20 +0100

    OnComponentHit Event

--- battleTank/Source/battleTank/Private/TankTrack.cpp
+++ battleTank/Source/battleTank/Private/TankTrack.cpp
@@ -4,6 +4,17 @@
 UTankTrack::UTankTrack()
 {
 	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void UTankTrack::BeginPlay()
+{
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * OtherComponent, FVector NormalImpulse, const FHitResult & Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("I'm hit!"))
 }
 void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
 {
@@ -20,6 +31,7 @@ void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompo
 	auto CorrectionForce = (TankRoot->GetMass()* CorrectionAcceleration) / 2; //Two tracks
 	TankRoot->AddForce(CorrectionForce);
 }
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto ForceApplied = GetForwardVector()*Throttle * TrackMaxDrivingForce;
--- battleTank/Source/battleTank/Public/TankTrack.h
+++ battleTank/Source/battleTank/Public/TankTrack.h
@@ -26,5 +26,10 @@ public:
 		float TrackMaxDrivingForce = 400000; //Assume 40 tonne tank, and 1g acceleration
 private:
 	UTankTrack();
+	virtual void BeginPlay() override;
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+	
 };
